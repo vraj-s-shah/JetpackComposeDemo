2a4f350d185d2a4f460efd088b1c82fdc0e68639
Vraj Shah <vraj.s@simformsolutions.com>
2023-01-25 11:23:58 +0530

    UNT-T13894: Initial setup with assets

---
+++ app/src/main/java/com/example/onlinelearning/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.onlinelearning.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val BlueText = Color(0xFF202244)
+val LightGrayText = Color(0xFF979797)
+val BaseGreen = Color(0xFF30C084)
