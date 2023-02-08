8875c42df44238e81dc2e730da76eff7d7e7beda
Vraj Shah <vraj.s@simformsolutions.com>
2023-02-08 17:27:23 +0530

    UNT-T13894: Created custom bottom navigation component

--- app/src/main/java/com/example/onlinelearning/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/onlinelearning/ui/home/HomeActivity.kt
@@ -4,18 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.onlinelearning.ui.base.BottomNavigationBar
+import com.example.onlinelearning.ui.theme.BottomNavigationBarShadowColor
+import com.example.onlinelearning.utils.extensions.shadow
+import com.example.onlinelearning.utils.navigation.BottomNavigationItem
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-                Text(text = "Home")
+            val navHostController = rememberNavController()
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(
+                        navHostController = navHostController,
+                        modifier = Modifier
+                            .shadow(
+                                color = BottomNavigationBarShadowColor,
+                                blurRadius = 20.dp,
+                                offsetX = 6.dp
+                            )
+                    )
+                }
+            ) { padding ->
+                Box(modifier = Modifier.padding(padding)) {
+                    HomeScreens(navHostController)
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun HomeScreens(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = BottomNavigationItem.Home.route) {
+        composable(BottomNavigationItem.Home.route) {
+
+        }
+        composable(BottomNavigationItem.Search.route) {
+
+        }
+        composable(BottomNavigationItem.MyCourses.route) {
+
+        }
+        composable(BottomNavigationItem.Message.route) {
+
+        }
+        composable(BottomNavigationItem.Profile.route) {
+
         }
     }
 }
--- app/src/main/java/com/example/onlinelearning/utils/extensions/CommonExtensions.kt
+++ app/src/main/java/com/example/onlinelearning/utils/extensions/CommonExtensions.kt
@@ -2,12 +2,21 @@ package com.example.onlinelearning.utils.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.BlurMaskFilter
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.onlinelearning.viewmodel.ViewModelFactory
@@ -64,3 +73,43 @@ fun FocusManager.moveLeft() = moveFocus(FocusDirection.Left)
  * Focus manager extension to move focus right
  */
 fun FocusManager.moveRight() = moveFocus(FocusDirection.Right)
+
+/**
+ * Custom shadow to apply x-offset, y-offset and blur radius
+ */
+fun Modifier.shadow(
+    color: Color = Color.Black,
+    borderRadius: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0f.dp,
+) = then(
+    drawBehind {
+        drawIntoCanvas {
+            val paint = Paint()
+            val frameworkPaint = paint.asFrameworkPaint()
+            val spreadPixel = spread.toPx()
+            val leftPixel = (0f - spreadPixel) + offsetX.toPx()
+            val topPixel = (0f - spreadPixel) + offsetY.toPx()
+            val rightPixel = (size.width + spreadPixel)
+            val bottomPixel = (size.height + spreadPixel)
+
+            if (blurRadius != 0.dp) {
+                frameworkPaint.maskFilter =
+                    (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
+            }
+
+            frameworkPaint.color = color.toArgb()
+            it.drawRoundRect(
+                left = leftPixel,
+                top = topPixel,
+                right = rightPixel,
+                bottom = bottomPixel,
+                radiusX = borderRadius.toPx(),
+                radiusY = borderRadius.toPx(),
+                paint = paint
+            )
+        }
+    }
+)
